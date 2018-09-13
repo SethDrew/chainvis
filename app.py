07691581c6fdacaaaccb46fcf0c6849822ae84be
annakap4 <anna.kaplan4@gmail.com>
2018-09-12 21:05:31 -0400

    colored grid

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 from flask import Flask
 from flask import render_template, request, jsonify
+from data import get_flip_totals
+import json
 
 app = Flask(__name__)
 
@@ -9,9 +11,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-    return render_template("index.html")
-
+	flips = get_flip_totals()
+	#flips = json.dumps(flips)
+	print(flips)
+	return render_template("index.html", flips=flips)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
--- data.py
+++ data.py
@@ -8,6 +8,30 @@ def get_flip_totals():
     flips = np.load("static/chainresults/flip_history.npy")
 
     flips = [tuple(x) for x in flips]
-    return dict(Counter(flips))
+    flips = dict(Counter(flips))
 
+    index_flips = dict()
+
+    for flip in flips.keys():
+    	index_flips[tuple_to_index(flip)] = flips[flip]
+
+    return index_flips
+
+
+def tuple_to_index(coord):
+	return coord[1]*10 + coord[0]
+
+
+from itertools import groupby
+from collections import Counter
+
+flips = np.load("static/chainresults/flip_history.npy")
+
+flips = [tuple(x) for x in flips]
+flips = dict(Counter(flips))
+
+index_flips = dict()
+
+for flip in flips.keys():
+	index_flips[tuple_to_index(flip)] = flips[flip]
 

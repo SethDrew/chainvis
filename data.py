057544a283c135b989179320fcf7663ffd2089da
Seth Drew <seth@sethdrew.com>
2018-09-12 20:00:25 -0400

    filtered flip counts

--- data.py
+++ data.py
@@ -1,37 +1,13 @@
 import numpy as np
 
-flips = np.load("static/chainresults/flip_history.npy")
+
+def get_flip_totals():
+    from itertools import groupby
+    from collections import Counter
+
+    flips = np.load("static/chainresults/flip_history.npy")
+
+    flips = [tuple(x) for x in flips]
+    return dict(Counter(flips))
 
 
-# filter later:
-flips
-from itertools import grouby
-from itertools import groupby
-flips = flips.tolist
-flips = flips.tolist()
-flips
-flips = np.load("static/chainresults/flip_history.npy")
-flips
-flips = flips.tolist()
-flips
-from itertools import groupby
-[len(list(group)) for key, group in groupby(flips)]
-from collections import Counter
-Counter(flips)
-len(flips)
-flips = np.load("static/chainresults/flip_history.npy")
-len(flips)
-flips = [tuple(x) for x in flips]
-flips
-Counter(flips)
-flip_counts = Counter(flips)
-flip_counts
-typeof(flip_counts)
-type(flip_counts)
-flip_counts.items()
-import readline
-for i in range(readline.get_current_history_length()):
-	print(readline.get_history_item(i+1)
-)
-for i in range(readline.get_current_history_length()):
-	print(readline.get_history_item(i+1))
